a884751b92af04152d298d7e98ee2ff029f85741
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 20:24:03 +0530

    Update min_max_from_list.py

--- min_max_from_list.py
+++ min_max_from_list.py
@@ -1,12 +1,20 @@
-n=int(input("enter values= "))
+ find the max and min value from a list
+n=int(input("enter value of n= "))
 if n<=0:
     print("{} is invalid input".format(n))
 else:
-    lst=[]
+    we=[]
     for i in range(1,n+1):
-        val=int(input("enter {} values=".format(i)))
-        lst.append(val)
+        val=int(input("enter {} value= ".format(i)))
+        we.append(val)
     else:
-        lst.sort()
-        print("min is {}".format(lst[0]))
-        print("max is {}".format(lst[-1]))
+        maxv=we[0]
+        minv=we[0]
+        for val in we:
+            if val>maxv:
+                maxv=val
+            if val<minv:
+                minv=val
+        else:
+            print("max={}".format(maxv))
+            print("maxv=%d" %minv)
