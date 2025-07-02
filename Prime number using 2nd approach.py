1d2e403a42f2dfbc16a638593f0d0941d597f0b8
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 19:30:55 +0530

    first commit

---
+++ Prime number using 2nd approach.py
@@ -0,0 +1,16 @@
+#2nd approach
+def prime():
+    ass="prime number"
+    for i in range(2,n):
+        if n%2==0:
+            ass="not prime number"
+            break
+    if ass=="prime number":
+        print("{} is prime number".format(n))
+    else:
+        print("{} is not prime number".format(n))
+
+n=int(input("Enter any number= "))
+if n<=1:
+    print("{} is invalid input".format(n))
+prime()
