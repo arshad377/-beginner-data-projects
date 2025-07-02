93a2d69e7fd79b6193035f18ba99e3e5be0dd1b4
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 20:16:08 +0530

    Create user_input_list.py

---
+++ user_input_list.py
@@ -0,0 +1,10 @@
+n=int(input("enter values= "))
+if n<=0:
+    print("{}invalid input".format(n))
+else:
+    a=list()
+    for i in range(0,n+1):
+        val=input("enter values= ")
+        a.append(val)
+    else:
+        print("{}".format(a))
