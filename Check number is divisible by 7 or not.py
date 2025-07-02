2481512acb72285f48599eec22c1c15674e51a67
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 20:36:02 +0530

    Create Check number is divisible by 7 or not.py

---
+++ Check number is divisible by 7 or not.py
@@ -0,0 +1,6 @@
+#write a python program to check whether the number is divisible by 7 or not.
+n=int(input("Enter any number= "))
+if n%7==0:
+    print("{} is divisible by 7".format(n))
+else:
+    print("{} is not divisible by 7".format(n))
