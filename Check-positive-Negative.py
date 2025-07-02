ffcca98f097d398f7108c568956e382c64a0d89d
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 19:36:28 +0530

    first commit

---
+++ Check-positive-Negative.py
@@ -0,0 +1,8 @@
+#find positive or negative of a number
+a=int(input("Enter value of a= "))
+if a>0:
+    print("{} is positive".format(a))
+if a<0:
+    print("{} is Negative".format(a))
+if a==0:
+    print("{} is zero".format(a))
