aac36571a47f202a2cd9aa72b6babc6279ef5f35
MD Arshad Hussain <75202488+arshad377@users.noreply.github.com>
2025-07-02 20:54:16 +0530

    Create Temperature_converter.py

---
+++ Temperature_converter.py
@@ -0,0 +1,8 @@
+#Fehrenheit to celsius and kelvin
+f=float(input("Enter temperature in fehrenheit= "))
+c=(f-32)*5/9
+k=(f-32)*5/9+273.15
+print("-"*50)
+print("Temperature in celsius=%0.2f" %c)
+print("Temperature in kelvin=%0.2f" %k)
+print("-"*50)
